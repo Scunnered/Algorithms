768fd883cf2bb864045e1f5cdb485c57065508d2
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-02 16:59:11 +0100

    algorithm iterates. Once run it mimics the commented line.
    Iterated 9 times and sorted correctly

--- bubble_sort_demo.py
+++ bubble_sort_demo.py
@@ -1,5 +1,12 @@
 def bubble_sort(arr):
-    pass
+    swap_happened = True
+    while swap_happened:
+        print('Bubble sort result ' +str(arr))
+        swap_happened = False
+        for num in range(len(arr)-1):
+            if arr[num] > arr[num+1]:
+                swap_happened = True
+                arr[num], arr[num+1] = arr[num+1], arr[num]
 
 l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
 bubble_sort(l)
