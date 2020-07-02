1f46fe308a4b9c11906faa144456d504ab3c83e3
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-02 16:33:33 +0100

    l to = unsorted list: 6 8 1 4 10 7 8 9 3 2 5

--- bubble_sort_demo.py
+++ bubble_sort_demo.py
@@ -1,1 +1,10 @@
-# test
+def bubble_sort(arr):
+    pass
+
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+bubble_sort(l)
+
+# Start Iteration 0:    6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5
+# After Iteration 1:    6, 1, 4, 8, 7, 8, 9, 3, 2, 5, 10
+# After Iteration 2:    1, 4, 6, 7, 8, 8, 3, 2, 5, 9, 10
+# Sorted list:          1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10
