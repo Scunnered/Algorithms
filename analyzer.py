cadcf9b824fe341f63be05b592ef706a989157fd
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-06 15:42:31 +0100

    Project phase 5: Add multiple run functionality and perform additional testing

--- analyzer.py
+++ analyzer.py
@@ -19,12 +19,17 @@ def analyze_func(func_name, arr):
     func_name(arr)
     toc = time.time()
     seconds = toc-tic
-    print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds}")
+    print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds:.5f}")
 
 size = int(input("What size list do you want to create? "))
 max = int(input("What is the max value of the range? "))
+run_times = int(input("How many times do you want to run? "))
 
-l = create_random_list(size,max)
-analyze_func(bubblesort, l.copy())
-analyze_func(quicksort, l)
-analyze_func(mergesort, l)
+for num in range(run_times):
+    print(f"Run: {num+1}")
+    l = create_random_list(size,max)
+    analyze_func(bubblesort, l.copy())
+    analyze_func(quicksort, l)
+    analyze_func(mergesort, l)
+    analyze_func(sorted, l)
+    print("-" * 40)
