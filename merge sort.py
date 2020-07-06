e48b9e0c859da0aeae719aeb84fcfe3f25293f7b
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-06 13:38:48 +0100

    compare first element in each list and append smaller elements
    Using a indices and an iterator perform same comparison for all elements in both lists
    
    Move marker up by 1 postion after smaller number is found
    
    Copy remaining list once comparisons are complete and there are items still remaining in one of the lists

--- merge sort.py
+++ merge sort.py
@@ -3,11 +3,17 @@ def merge_sorted(arr1,arr2):
     print(f"left: {arr1} and right: {arr2}")
     sorted_arr = []
     i, j = 0, 0
+    if arr[i] < arr2[j]:
+        sorted_arr.append(arr1[i])
+        i += 1
+    else:
+        sorted_arr.append(arr2[j])
+        j += 1
     print(f"Left list index i is {i} and has value: {arr1[i]}")
     print(f"Right list index j is {j} and has value: {arr2[j]}")
     return sorted_arr
 
-# xxxxxxxxxxxxxxxx Program Execution xxxxxxxxxxxxxxxx
-l1 = [1,4,6,8,10]
-l2 = [2,3,5,7,8,9]
-print(f"Un-merged list: {merge_sorted(l1,l2)}")
+# Program execution
+l1 = [2,4,6,8,10]
+l2 = [1,3,5,7,8,9]
+print(f"Un-merged list: {merge_sorted (l1,l2)}")
