b6020047af65055f0eb479a636a397f818128ff1
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-06 13:58:46 +0100

    further merge functionality added

--- merge sort.py
+++ merge sort.py
@@ -1,20 +1,25 @@
 def merge_sorted(arr1,arr2):
-    print("Merge function called with lists below:")
+    print("Merge function called with the lists below")
     print(f"left: {arr1} and right: {arr2}")
     sorted_arr = []
     i, j = 0, 0
-
-    while i < len(arr1) and j < len(arr2)
-        if arr[i] < arr2[j]:
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
             sorted_arr.append(arr1[i])
             i += 1
         else:
             sorted_arr.append(arr2[j])
             j += 1
         print(sorted_arr)
+    while i < len(arr1):
+        sorted_arr.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        sorted_arr.append(arr2[j])
+        j += 1
     return sorted_arr
 
 # Program execution
-l1 = [2,4,6,8]
-l2 = [1,3,5,7,8,9,10]
-print(f"Merged list: {merge_sorted (l1,l2)}")
+l1 = [1,3,5,7,8,9,10]
+l2 = []
+print(f"Merged list: {merge_sorted(l1,l2)}")
