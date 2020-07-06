8aa8b2d4a0387800d529bb43423583cb579f4677
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-06 13:45:11 +0100

    further merge functionality

--- merge sort.py
+++ merge sort.py
@@ -3,17 +3,18 @@ def merge_sorted(arr1,arr2):
     print(f"left: {arr1} and right: {arr2}")
     sorted_arr = []
     i, j = 0, 0
-    if arr[i] < arr2[j]:
-        sorted_arr.append(arr1[i])
-        i += 1
-    else:
-        sorted_arr.append(arr2[j])
-        j += 1
-    print(f"Left list index i is {i} and has value: {arr1[i]}")
-    print(f"Right list index j is {j} and has value: {arr2[j]}")
+
+    while i < len(arr1) and j < len(arr2)
+        if arr[i] < arr2[j]:
+            sorted_arr.append(arr1[i])
+            i += 1
+        else:
+            sorted_arr.append(arr2[j])
+            j += 1
+        print(sorted_arr)
     return sorted_arr
 
 # Program execution
-l1 = [2,4,6,8,10]
-l2 = [1,3,5,7,8,9]
-print(f"Un-merged list: {merge_sorted (l1,l2)}")
+l1 = [2,4,6,8]
+l2 = [1,3,5,7,8,9,10]
+print(f"Merged list: {merge_sorted (l1,l2)}")
