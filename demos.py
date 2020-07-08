ceb99e9a41ff84044d4bf2752e07fcaedb91be2d
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-08 11:23:52 +0100

    Bisec search added. demos to reflect new project.

--- demos.py
+++ demos.py
@@ -1,52 +1,36 @@
-print("Algorithms file loaded")
+import time
+from random import choice
+from string import ascii_lowercase as letters
 
-def quicksort(arr):
-    if len(arr) < 2:
-        return arr
-    else:
-        pivot = arr[-1]
-        smaller, equal, larger = [], [], []
-        for num in arr:
-            if num < pivot:
-                smaller.append(num)
-            elif num == pivot:
-                equal.append(num)
-            else:
-                larger.append(num)
-        return quicksort(smaller) + equal + quicksort(larger)
-
-def merge_sorted(arr1,arr2):
-    sorted_arr = []
-    i, j = 0, 06
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] < arr2[j]:
-            sorted_arr.append(arr1[i])
-            i += 1
+def bisection_iter(n, arr):
+    start = 0
+    stop = len(arr)-1
+    while start <= stop:
+        mid = (start + stop)//2
+        if n == arr[mid]:
+            return mid, f"{n} found at index: {mid}" # return tuple
+        elif n > arr[mid]:
+            start = mid + 1
         else:
-            sorted_arr.append(arr2[j])
-            j += 1
-    while i < len(arr1):
-        sorted_arr.append(arr1[i])
-        i += 1
-    while j < len(arr2):
-        sorted_arr.append(arr2[j])
-        j += 1
-    return sorted_arr
-    ddtsz
-def mergesort(arr):
-    if len(arr) < 2:
-        return arr[:]
-    else:
-        middle = len(arr)//2
-        l1 = mergesort(arr[:middle])
-        l2 = mergesort(arr[middle:])
-        return merge_sorted(l1, l2)
+            stop = mid - 1
+    return None, f"{n} not found in list"
 
-def bubblesort(arr):
-    swap_happened = True
-    while swap_happened:
-        swap_happened = False
-        for num in range(len(arr)-1):
-            if arr[num] > arr[num+1]:
-                swap_happened = True
-                arr[num], arr[num+1] = arr[num+1], arr[num]
+def analyze_func(func_name, *args):
+    tic = time.time()
+    func_name(*args) # accept variable list of arguments
+    toc = time.time()
+    seconds = toc-tic
+    print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds:.5f}")
+
+###### functions to generate lists of random emails ######
+def generate_name(length_of_name):
+    return ''.join(choice(letters) for i in range(length_of_name))
+
+def get_domain(list_of_domains):
+    return choice(list_of_domains)
+
+def generate_emails(length_of_name, list_of_domains, total_emails):
+    emails = []
+    for num in range(total_emails):
+        emails.append(generate_name(length_of_name)+"@"+get_domain(list_of_domains))
+    return emails
