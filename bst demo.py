6e42cce3754676527eebfe8571df0ebc123ac12c
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-07-08 13:36:55 +0100

    BST from scratch - search - delete demo - deleting nodes with 1 child - deleting nodes with 2 children

--- bst demo.py
+++ bst demo.py
@@ -1,3 +1,5 @@
+# Final implementation
+
 class Node:
     def __init__(self, key):
         self.data = key
@@ -39,12 +41,14 @@ class BSTDemo:
             self._in_order(curr.right_child)
 
     def pre_order(self):
+        '''root, left, right'''
         pass
 
     def _pre_order(self, curr):
         pass
 
     def post_order(self):
+        '''left, right, root'''
         pass
 
     def _post_order(self, curr):
@@ -63,19 +67,74 @@ class BSTDemo:
                 return self._find_val(curr.right_child, key)
         return "Value not found in tree"
 
+    def min_right_subtree(self, curr):
+        if curr.left_child == None:
+            return curr
+        else:
+            return self.min_right_subtree(curr.left_child)
+
     def delete_val(self, key):
-        pass
+        self._delete_val(self.root, None, None, key)
 
     def _delete_val(self, curr, prev, is_left, key):
-        pass
+        if curr:
+            if key == curr.data:
+                if curr.left_child and curr.right_child:
+                    min_child = self.min_right_subtree(curr.right_child)
+                    curr.data = min_child.data
+                    self._delete_val(curr.right_child, curr, False, min_child.data)
+                elif curr.left_child == None and curr.right_child == None:
+                    if prev:
+                        if is_left:
+                            prev.left_child = None
+                        else:
+                            prev.right_child = None
+                    else:
+                        self.root = None
+                elif curr.left_child == None:
+                    if prev:
+                        if is_left:
+                            prev.left_child = curr.right_child
+                        else:
+                            prev.right_child = curr.right_child
+                    else:
+                        self.root = curr.right_child
+                else:
+                    if prev:
+                        if is_left:
+                            prev.left_child = curr.left_child
+                        else:
+                            prev.right_child = curr.left_child
+                    else:
+                        self.root = curr.left_child
+            elif key < curr.data:
+                self._delete_val(curr.left_child, curr, True, key)
+            elif key > curr.data:
+                self._delete_val(curr.right_child, curr, False, key)
+        else:
+            print(f"{key} not found in Tree")
 
 tree = BSTDemo()
-tree.insert("H")
-tree.insert("D")
-tree.insert("I")
-tree.insert("M")
-tree.insert("J")
-tree.insert("L")
+tree.insert("F")
+tree.insert("C")
+print("Test deleting leaf node which is left child of parent")
+tree.in_order()
+tree.delete_val("C")
+tree.in_order()
+tree.insert("G")
+print("Test deleting leaf node which is right child of parent")
+tree.in_order()
+tree.delete_val("G")
+tree.in_order()
+tree.insert("A")
+print("Test deleting parent/root node which has one child")
+tree.in_order()
+tree.delete_val("F")
+tree.in_order()
+print("Test deleting root node which has no children")
+tree.in_order()
+tree.delete_val("A")
+tree.in_order()
 tree.insert("F")
 tree.insert("C")
 tree.insert("G")
@@ -83,7 +142,19 @@ tree.insert("A")
 tree.insert("B")
 tree.insert("K")
 tree.insert("E")
+tree.insert("H")
+tree.insert("D")
+tree.insert("I")
+tree.insert("M")
+tree.insert("J")
+tree.insert("L")
 tree.in_order()
-print(tree.find_val("E"))
-print(tree.find_val("J"))
-print(tree.find_val("Z"))
+tree.delete_val("F")
+tree.in_order()
+tree.in_order()
+tree.delete_val("K")
+tree.in_order()
+tree.in_order()
+tree.delete_val("C")
+tree.in_order()
+tree.delete_val("Z")
